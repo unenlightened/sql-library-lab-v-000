544b252a8645224bfb6ecd0486480b9bae91d324
Chely Ho <adept_inept@yahoo.com>
2018-03-02 19:00:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -45,7 +45,7 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   <<-DOC
-  SELECT characters.name, SUM(books.id) AS book_appearances from characters
+  SELECT characters.name, COUNT(books.id) AS book_appearances from characters
   JOIN books ON books.series_id = characters.series_id
   GROUP BY characters.name
   DOC
